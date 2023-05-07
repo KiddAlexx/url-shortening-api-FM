a9455b9f0831e0d2cc1e6ad711bfab8eece64a9f
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-07 08:53:42 +0200

    create UrlForm component with basic html markup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from '../src/components/Header';
 import Hero from '../src/components/Hero';
+import UrlForm from './components/UrlForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
+      <UrlForm />
     </div>
   );
 }
---
+++ src/components/UrlForm.jsx
@@ -0,0 +1,12 @@
+function UrlForm() {
+  return (
+    <main className="url-app-container">
+      <form action="" className="url-shortening-form">
+        <input type="text" className="url-entry" />
+        <button className="btn-main">Shorten It!</button>
+      </form>
+    </main>
+  );
+}
+
+export default UrlForm;
