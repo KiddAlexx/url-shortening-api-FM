5dfcd046000d7bbe7db9b99ff0a74907a680cab2
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-07 10:21:28 +0200

    add background image to url form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
-import Header from '../src/components/Header';
-import Hero from '../src/components/Hero';
+import Header from './components/Header';
+import Hero from './components/Hero';
 import UrlForm from './components/UrlForm';
 
 function App() {
