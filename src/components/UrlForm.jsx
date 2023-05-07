2ffd0cefd2ac636bccea670a1d37fc265acca345
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-07 09:20:18 +0200

    add text styling for url input field and button

--- src/components/UrlForm.jsx
+++ src/components/UrlForm.jsx
@@ -2,8 +2,13 @@ function UrlForm() {
   return (
     <main className="url-app-container">
       <form action="" className="url-shortening-form">
-        <input type="text" className="url-entry" />
-        <button className="btn-main">Shorten It!</button>
+        <input
+          type="text"
+          placeholder="Shorten a link here..."
+          name="urlFull"
+          className="url-input-field"
+        />
+        <button className="btn-main btn-shorten">Shorten It!</button>
       </form>
     </main>
   );
