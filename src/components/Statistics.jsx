7889fd0f6a05ba2c2d349d78aa2f5c6b394e2a90
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-10 08:06:19 +0200

    add html markup for Statistics header + description

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,5 +1,13 @@
 function Statistic() {
-  return <div className="statistics">Test</div>;
+  return (
+    <section className="statistics-container">
+      <h2 className="statistics-heading">Advanced Statistics</h2>
+      <p className="statistics-description">
+        Track how your links are performing across the web with our advanced
+        statistics dashboard.
+      </p>
+    </section>
+  );
 }
 
 export default Statistic;
