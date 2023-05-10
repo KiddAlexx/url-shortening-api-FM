8240d0440b38827ff8867742b9f6fcc59a861c29
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-10 09:10:22 +0200

    add info card html markup + pass content via props from Features component

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,4 +1,7 @@
 import InfoCard from './InfoCard';
+import iconBrandRecognition from '../assets/imgs/icon-brand-recognition.svg';
+import iconDetailedRecords from '../assets/imgs/icon-detailed-records.svg';
+import iconFullyCustomizable from '../assets/imgs/icon-fully-customizable.svg';
 
 function Features() {
   return (
@@ -10,9 +13,21 @@ function Features() {
           statistics dashboard.
         </p>
       </div>
-      <InfoCard />
-      <InfoCard />
-      <InfoCard />
+      <InfoCard
+        infoIcon={iconBrandRecognition}
+        infoHeading="Brand Recognition"
+        infoContent="Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content."
+      />
+      <InfoCard
+        infoIcon={iconDetailedRecords}
+        infoHeading="Detailed Records"
+        infoContent="Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions."
+      />
+      <InfoCard
+        infoIcon={iconFullyCustomizable}
+        infoHeading="Fully Customizable"
+        infoContent="Improve brand awareness and content discoverability through customizable links, supercharging audience engagement."
+      />
     </section>
   );
 }
--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,5 +1,15 @@
-function InfoCard() {
-  return <div>Test</div>;
+function InfoCard(props) {
+  return (
+    <div className="info-card">
+      <img
+        className="info-card-icon"
+        src={props.infoIcon}
+        alt={`Small icon representing ${props.infoHeading}`}
+      />
+      <h3 className="info-card-heading">{props.infoHeading}</h3>
+      <p className="info-card-content">{props.infoContent}</p>
+    </div>
+  );
 }
 
 export default InfoCard;
