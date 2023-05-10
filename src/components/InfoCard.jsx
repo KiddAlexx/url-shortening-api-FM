666a451e7efda07ec73e340f96223d755a56e5c7
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-10 09:42:19 +0200

    style info card icon

--- src/components/InfoCard.jsx
+++ src/components/InfoCard.jsx
@@ -1,11 +1,13 @@
 function InfoCard(props) {
   return (
     <div className="info-card">
-      <img
-        className="info-card-icon"
-        src={props.infoIcon}
-        alt={`Small icon representing ${props.infoHeading}`}
-      />
+      <div className="info-card-icon-container">
+        <img
+          className="info-card-icon"
+          src={props.infoIcon}
+          alt={`Small icon representing ${props.infoHeading}`}
+        />
+      </div>
       <h3 className="info-card-heading">{props.infoHeading}</h3>
       <p className="info-card-content">{props.infoContent}</p>
     </div>
