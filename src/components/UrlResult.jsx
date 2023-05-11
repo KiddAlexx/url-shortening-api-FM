b1622a486ef02d666dfc74f62a2d94f754117de3
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-11 15:45:32 +0200

    install framer motion + add basic animation to link elements as they are removed

--- src/components/UrlResult.jsx
+++ src/components/UrlResult.jsx
@@ -1,6 +1,20 @@
+import { motion } from 'framer-motion';
+
 function UrlResult(props) {
   const linkElements = props.urlData.map((urlObject) => (
-    <div className="link-container" key={urlObject.id}>
+    <motion.div
+      className="link-container"
+      key={urlObject.id}
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{
+        type: 'spring',
+        stiffness: 600,
+        damping: 30,
+      }}
+    >
       <a className="url-original" href={`http://${urlObject.urlOriginal}`}>
         {urlObject.urlOriginal}
       </a>
@@ -24,9 +38,10 @@ function UrlResult(props) {
           {urlObject.copied ? 'Copied!' : 'Copy'}
         </button>
       </div>
-    </div>
+    </motion.div>
   ));
 
   return <div>{linkElements}</div>;
 }
+
 export default UrlResult;
