a507a5fe9b2895f0e50c50967168cb81328f1119
KiddAlexx <98313951+KiddAlexx@users.noreply.github.com>
2023-05-15 21:33:26 +0200

    wrap menu navigation and auth buttons in container in prep for use in mobile menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,34 +3,36 @@ import shortlyLogo from '../assets/imgs/logo.svg';
 function Header() {
   return (
     <nav className="main-nav">
-      <div className="nav-left">
-        <img
-          src={shortlyLogo}
-          alt="Simple logo of company name - Shortly"
-          className="logo"
-        />
-        <ul className="nav-menu-items">
+      <img
+        src={shortlyLogo}
+        alt="Simple logo of company name - Shortly"
+        className="logo"
+      />
+      <div className="menu-auth-container">
+        <div className="nav-left">
+          <ul className="nav-menu-items">
+            <li>
+              <a href="#">Features</a>
+            </li>
+            <li>
+              <a href="#">Pricing</a>
+            </li>
+            <li>
+              <a href="#">Resources</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="auth-buttons">
           <li>
-            <a href="#">Features</a>
+            <a href="#" className="btn-login">
+              Login
+            </a>
           </li>
           <li>
-            <a href="#">Pricing</a>
-          </li>
-          <li>
-            <a href="#">Resources</a>
+            <button className="btn-signup btn-main">Sign Up</button>
           </li>
         </ul>
       </div>
-      <ul className="auth-buttons">
-        <li>
-          <a href="#" className="btn-login">
-            Login
-          </a>
-        </li>
-        <li>
-          <button className="btn-signup btn-main">Sign Up</button>
-        </li>
-      </ul>
     </nav>
   );
 }
